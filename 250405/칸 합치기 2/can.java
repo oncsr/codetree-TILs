a467272a3f73289bdc446e02fdcb0b4406615e84
oncsr <khj20006@naver.com>
2025-04-05 23:56:46 +0900

    Title: 칸 합치기 2, Time: 357ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 250405/칸 합치기 2/can.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int N, M;
+	static int[] r;
+	
+	static int f(int x) {return x==r[x] ? x : (r[x]=f(r[x]));}
+	
+	public static void main(String[] args) throws Exception {
+			
+		ready();
+		solve();
+	
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		N = nextInt();
+		M = nextInt();
+		r = new int[N+1];
+		for(int i=1;i<=N;i++) r[i] = i;
+		
+	}
+	
+	static void solve() throws Exception{
+		
+		int ans = N;
+		while(M-->0) {
+			int a = nextInt(), b = nextInt();
+			while(f(a) != f(b)) {
+				int x = f(a), y = f(x+1);
+				r[x] = y;
+				ans--;
+			}
+			bw.write(ans + "\n");
+		}
+		
+	}
+	
+}
