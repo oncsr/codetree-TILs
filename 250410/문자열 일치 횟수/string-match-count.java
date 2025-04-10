dc246207989b0c40359298a94ec45a6c4b504453
oncsr <khj20006@naver.com>
2025-04-10 11:45:49 +0900

    Title: 문자열 일치 횟수, Time: 136ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250410/문자열 일치 횟수/string-match-count.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	// IO field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st = new StringTokenizer("");
+
+	static void nextLine() throws Exception {st = new StringTokenizer(br.readLine());}
+	static String nextToken() throws Exception {
+		while(!st.hasMoreTokens()) nextLine();
+		return st.nextToken();
+	}
+	static int nextInt() throws Exception { return Integer.parseInt(nextToken()); }
+	static long nextLong() throws Exception { return Long.parseLong(nextToken()); }
+	static double nextDouble() throws Exception { return Double.parseDouble(nextToken()); }
+	static void bwEnd() throws Exception {bw.flush();bw.close();}
+	
+	// Additional field
+	
+	static int N;
+	static char[] T, P; 
+	static int[] X;
+	
+	public static void main(String[] args) throws Exception {
+		
+		ready();
+		solve();
+	
+		bwEnd();
+		
+	}
+	
+	static void ready() throws Exception{
+		
+		N = nextInt();
+		P = nextToken().toCharArray();
+		T = new char[2*N-1];
+		char[] temp = nextToken().toCharArray();
+		for(int i=0;i<2*N-1;i++) T[i] = temp[i%N];
+		X = new int[N];
+		
+	}
+	
+	static void solve() throws Exception{
+		
+		for(int i=1;i<N;i++) {
+			X[i] = X[i-1];
+			while(X[i]>0 && P[X[i]] != P[i]) X[i] = X[X[i]-1];
+			if(P[X[i]] == P[i]) X[i]++;
+		}
+		
+		int ans = 0;
+		for(int i=0,j=0;i<2*N-1;i++) {
+			while(j>0 && P[j] != T[i]) j = X[j-1];
+			if(P[j] == T[i]) j++;
+			if(j == N) {
+				ans++;
+				j = X[j-1];
+			}
+		}
+		bw.write(ans + "\n");
+		
+	}
+	
+}
